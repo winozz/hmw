88bb305e19d6506f52d91b65748a5f31dc7349d4
winozz <godwinlorenzllabres@gmail.com>
2025-09-17 12:08:43 +0800

    add github pages config

--- medtech-celebration/vite.config.js
+++ medtech-celebration/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/hmw/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
